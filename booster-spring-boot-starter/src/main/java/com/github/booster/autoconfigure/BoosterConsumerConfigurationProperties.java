3b27738e6a4f1c7f1e643a602ba3c3d7ff723e24
Yujiumin <934639947@qq.com>
2020-09-25 01:40:02 +0800

    消费者配置

---
+++ booster-spring-boot-starter/src/main/java/com/github/booster/autoconfigure/BoosterConsumerConfigurationProperties.java
@@ -0,0 +1,77 @@
+package com.github.booster.autoconfigure;
+
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 消费者配置
+ *
+ * @author Yujiumin
+ * @version 2020/09/25
+ */
+@Configuration
+@ConfigurationProperties("booster.consumer")
+public class BoosterConsumerConfigurationProperties {
+
+    private Boolean enabled = false;
+
+    private String instanceName = "BOOSTER_CONSUMER";
+
+    private Integer consumeThreadMin = 10;
+
+    private Integer consumeThreadMax = 20;
+
+    private MessageModel messageModel = MessageModel.CLUSTERING;
+
+    private ConsumeFromWhere consumeFromWhere = ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET;
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    public void setInstanceName(String instanceName) {
+        this.instanceName = instanceName;
+    }
+
+    public Integer getConsumeThreadMin() {
+        return consumeThreadMin;
+    }
+
+    public void setConsumeThreadMin(Integer consumeThreadMin) {
+        this.consumeThreadMin = consumeThreadMin;
+    }
+
+    public Integer getConsumeThreadMax() {
+        return consumeThreadMax;
+    }
+
+    public void setConsumeThreadMax(Integer consumeThreadMax) {
+        this.consumeThreadMax = consumeThreadMax;
+    }
+
+    public MessageModel getMessageModel() {
+        return messageModel;
+    }
+
+    public void setMessageModel(MessageModel messageModel) {
+        this.messageModel = messageModel;
+    }
+
+    public ConsumeFromWhere getConsumeFromWhere() {
+        return consumeFromWhere;
+    }
+
+    public void setConsumeFromWhere(ConsumeFromWhere consumeFromWhere) {
+        this.consumeFromWhere = consumeFromWhere;
+    }
+}
