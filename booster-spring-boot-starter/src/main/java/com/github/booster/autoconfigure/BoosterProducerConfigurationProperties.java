02f030c7d77af8e63786657f746b70a9830fad80
Yujiumin <934639947@qq.com>
2020-09-25 01:40:20 +0800

    生产者配置

---
+++ booster-spring-boot-starter/src/main/java/com/github/booster/autoconfigure/BoosterProducerConfigurationProperties.java
@@ -0,0 +1,55 @@
+package com.github.booster.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 生产者配置
+ *
+ * @author Yujiumin
+ * @version 2020/09/25
+ */
+@Configuration
+@ConfigurationProperties("booster.producer")
+public class BoosterProducerConfigurationProperties {
+
+    private Boolean enabled = false;
+
+    private String instanceName = "BOOSTER_PRODUCER";
+
+    private Integer retryTimesWhenSendAsyncFailed = 3;
+
+    private Boolean vipChannelEnabled = false;
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    public void setInstanceName(String instanceName) {
+        this.instanceName = instanceName;
+    }
+
+    public Integer getRetryTimesWhenSendAsyncFailed() {
+        return retryTimesWhenSendAsyncFailed;
+    }
+
+    public void setRetryTimesWhenSendAsyncFailed(Integer retryTimesWhenSendAsyncFailed) {
+        this.retryTimesWhenSendAsyncFailed = retryTimesWhenSendAsyncFailed;
+    }
+
+    public Boolean getVipChannelEnabled() {
+        return vipChannelEnabled;
+    }
+
+    public void setVipChannelEnabled(Boolean vipChannelEnabled) {
+        this.vipChannelEnabled = vipChannelEnabled;
+    }
+}
