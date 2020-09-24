0ce6c739336ff9eac5d7d1d7cfdc70688b06cbb1
Yujiumin <934639947@qq.com>
2020-09-24 22:23:20 +0800

    API联动修改

--- booster-core/src/main/java/com/github/booster/core/consumer/BoosterConsumer.java
+++ booster-core/src/main/java/com/github/booster/core/consumer/BoosterConsumer.java
@@ -1,15 +1,14 @@
 package com.github.booster.core.consumer;
 
-import com.github.booster.core.builder.AbstractBuilder;
 import com.github.booster.common.constant.BoosterConstant;
 import com.github.booster.common.exception.BoosterException;
 import com.github.booster.common.util.ObjectUtils;
 import com.github.booster.common.util.StringUtils;
+import com.github.booster.core.builder.AbstractBuilder;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
-import org.apache.rocketmq.remoting.exception.RemotingConnectException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,12 +29,12 @@ public class BoosterConsumer {
     public BoosterConsumer(Builder builder) {
         try {
             consumer = new DefaultMQPushConsumer(builder.groupName);
-            consumer.setInstanceName(ObjectUtils.isNull(builder.instanceName, StringUtils.link("_", builder.groupName.toUpperCase(), "CONSUMER")));
             consumer.setNamesrvAddr(builder.nameServerAddr);
-            consumer.setConsumeThreadMin(ObjectUtils.isNull(builder.consumeThreadMin, consumer.getConsumeThreadMin()));
-            consumer.setConsumeThreadMax(ObjectUtils.isNull(builder.consumeThreadMax, consumer.getConsumeThreadMax()));
-            consumer.setConsumeFromWhere(ObjectUtils.isNull(builder.consumeFromWhere, consumer.getConsumeFromWhere()));
-            consumer.setMessageModel(ObjectUtils.isNull(builder.messageModel, consumer.getMessageModel()));
+            consumer.setInstanceName(ObjectUtils.nullOrElse(builder.instanceName, StringUtils.link("_", builder.groupName.toUpperCase(), "CONSUMER")));
+            consumer.setConsumeThreadMin(ObjectUtils.nullOrElse(builder.consumeThreadMin, consumer.getConsumeThreadMin()));
+            consumer.setConsumeThreadMax(ObjectUtils.nullOrElse(builder.consumeThreadMax, consumer.getConsumeThreadMax()));
+            consumer.setConsumeFromWhere(ObjectUtils.nullOrElse(builder.consumeFromWhere, consumer.getConsumeFromWhere()));
+            consumer.setMessageModel(ObjectUtils.nullOrElse(builder.messageModel, consumer.getMessageModel()));
             consumer.registerMessageListener(new MessageDispatcher(builder.messageHandlers));
 
             for (Subscription subscription : builder.subscriptions) {
@@ -57,7 +56,6 @@ public class BoosterConsumer {
             e.printStackTrace();
         }
     }
-
 
     public static class Builder extends AbstractBuilder<BoosterConsumer> {
         private String groupName;
