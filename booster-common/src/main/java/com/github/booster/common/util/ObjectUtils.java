8e4a80e6df6842f993287bade47bb4e74eacc66b
Yujiumin <934639947@qq.com>
2020-09-24 22:30:53 +0800

    使用Optional做默认值

--- booster-common/src/main/java/com/github/booster/common/util/ObjectUtils.java
+++ booster-common/src/main/java/com/github/booster/common/util/ObjectUtils.java
@@ -1,6 +1,6 @@
 package com.github.booster.common.util;
 
-import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author Yujiumin
@@ -16,8 +16,8 @@ public class ObjectUtils {
      * @param <T>          被判断对象泛型
      * @return 最终值
      */
-    public static <T> T isNull(T obj, T defaultValue) {
-        return Objects.isNull(obj) ? defaultValue : obj;
+    public static <T> T nullOrElse(T obj, T defaultValue) {
+        return Optional.ofNullable(obj).orElse(defaultValue);
     }
 
 }
