661384f7718f8d699b049308d935331ee61aca74
Yujiumin <934639947@qq.com>
2020-09-26 00:57:30 +0800

    消费者自动配置

--- booster-spring-boot-starter/src/main/java/com/github/booster/autoconfigure/BoosterConsumerAutoConfiguration.java
+++ booster-spring-boot-starter/src/main/java/com/github/booster/autoconfigure/BoosterConsumerAutoConfiguration.java
@@ -1,19 +1,19 @@
 package com.github.booster.autoconfigure;
 
+import com.github.booster.annotation.Consumer;
 import com.github.booster.common.util.CollectionUtils;
 import com.github.booster.core.consumer.BoosterConsumer;
 import com.github.booster.core.consumer.MessageHandler;
 import com.github.booster.core.consumer.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * 消费者自动配置
@@ -26,13 +26,16 @@ import java.util.Objects;
 @ComponentScan("com.github.booster.autoconfigure")
 public class BoosterConsumerAutoConfiguration implements InitializingBean {
 
+    private Logger logger = LoggerFactory.getLogger(BoosterConsumerAutoConfiguration.class);
+
     private BoosterConfigurationProperties configurationProperties;
 
     private BoosterConsumerConfigurationProperties consumerConfigurationProperties;
 
     private List<MessageHandler> messageHandlerList;
 
-    public BoosterConsumerAutoConfiguration(BoosterConfigurationProperties configurationProperties, BoosterConsumerConfigurationProperties consumerConfigurationProperties) {
+    public BoosterConsumerAutoConfiguration(BoosterConfigurationProperties configurationProperties,
+                                            BoosterConsumerConfigurationProperties consumerConfigurationProperties) {
         this.configurationProperties = configurationProperties;
         this.consumerConfigurationProperties = consumerConfigurationProperties;
     }
@@ -41,13 +44,15 @@ public class BoosterConsumerAutoConfiguration implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         Map<String, List<MessageHandler>> messageHandlerMap = divideMessageHandlerByGroup();
         for (String group : messageHandlerMap.keySet()) {
+            List<MessageHandler> currentGroupMessageHandlers = messageHandlerMap.get(group);
             BoosterConsumer.Builder boosterConsumerBuilder = new BoosterConsumer.Builder(group);
             boosterConsumerBuilder.setNameServerAddr(configurationProperties.getNameServerAddr());
             boosterConsumerBuilder.setConsumeThreadMin(consumerConfigurationProperties.getConsumeThreadMin());
             boosterConsumerBuilder.setConsumeThreadMax(consumerConfigurationProperties.getConsumeThreadMax());
             boosterConsumerBuilder.setConsumeFromWhere(consumerConfigurationProperties.getConsumeFromWhere());
             boosterConsumerBuilder.setMessageModel(consumerConfigurationProperties.getMessageModel());
-            boosterConsumerBuilder.setSubscriptions(classifySubscription(messageHandlerMap.get(group)));
+            boosterConsumerBuilder.setSubscriptions(classifySubscription(currentGroupMessageHandlers));
+            boosterConsumerBuilder.registerMessageHandlers(currentGroupMessageHandlers.toArray(new MessageHandler[0]));
             BoosterConsumer boosterConsumer = boosterConsumerBuilder.build();
             boosterConsumer.listen();
         }
@@ -56,12 +61,21 @@ public class BoosterConsumerAutoConfiguration implements InitializingBean {
     /**
      * 把消费者Group分组
      *
-     * @return Map<String, List<MessageHandler>>
+     * @return Map<String, List < MessageHandler>>
      */
     private Map<String, List<MessageHandler>> divideMessageHandlerByGroup() {
         Map<String, List<MessageHandler>> messageHandlerMap = new LinkedHashMap<>();
-        if (!Objects.isNull(messageHandlerList) && !CollectionUtils.isEmpty(messageHandlerList)) {
-            // TODO: 2020/09/25 消费者分组
+        if (!CollectionUtils.isEmpty(messageHandlerList)) {
+            for (MessageHandler messageHandler : messageHandlerList) {
+                Class<? extends MessageHandler> messageHandlerClass = messageHandler.getClass();
+                Consumer annotation = messageHandlerClass.getAnnotation(Consumer.class);
+                String group = annotation.group();
+                if (messageHandlerMap.containsKey(group)) {
+                    messageHandlerMap.get(group).add(messageHandler);
+                } else {
+                    messageHandlerMap.put(group, CollectionUtils.list(true, messageHandler));
+                }
+            }
         }
         return messageHandlerMap;
     }
@@ -73,8 +87,29 @@ public class BoosterConsumerAutoConfiguration implements InitializingBean {
      * @return 订阅信息
      */
     private Subscription[] classifySubscription(List<MessageHandler> messageHandlerList) {
-        // TODO: 2020/09/25 订阅信息分类
-        return null;
+        List<Subscription> subscriptionList = new LinkedList<>();
+        Map<String, List<String>> topicTagMap = new LinkedHashMap<>();
+
+        for (MessageHandler messageHandler : messageHandlerList) {
+            Class<? extends MessageHandler> messageHandlerClass = messageHandler.getClass();
+            Consumer annotation = messageHandlerClass.getAnnotation(Consumer.class);
+            String topic = annotation.topic();
+            List<String> tagList = Arrays.asList(annotation.tags());
+            if (topicTagMap.containsKey(topic)) {
+                topicTagMap.get(topic).addAll(tagList);
+            } else {
+                topicTagMap.put(topic, tagList);
+            }
+        }
+
+        for (String topic : topicTagMap.keySet()) {
+            Subscription subscription = new Subscription();
+            subscription.setTopic(topic);
+            subscription.setTags(topicTagMap.get(topic).toArray(new String[0]));
+            subscriptionList.add(subscription);
+        }
+
+        return subscriptionList.toArray(new Subscription[0]);
     }
 
     @Autowired(required = false)
