387266d6c0d8b7de694fa075e6bf002fa792cc1b
Yujiumin <934639947@qq.com>
2020-09-26 00:34:36 +0800

    添加无参构造方法以及setter方法

--- booster-core/src/main/java/com/github/booster/core/consumer/Subscription.java
+++ booster-core/src/main/java/com/github/booster/core/consumer/Subscription.java
@@ -12,13 +12,25 @@ public class Subscription {
 
     private String[] tags;
 
+    public Subscription(){
+
+    }
+
     public Subscription(String topic, String... tags) {
         this.topic = topic;
         this.tags = tags;
     }
 
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
     public String getTopic() {
         return topic;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
     }
 
     public String[] getTags() {
